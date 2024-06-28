8914cb5f7a67781461771b9a35ddaa224efb6b82
mami.teshima <85916526+mami-tus@users.noreply.github.com>
2024-06-28 14:44:34 +0900

    feat: ✨  your-first-component

---
+++ describing-the-ui/src/app/components/Gallery.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Profile from './Profile';
+
+function Gallery() {
+  return (
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
+
+export default Gallery;
---
+++ describing-the-ui/src/app/your-first-component/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Gallery from '../components/Gallery';
+
+const YourFirstComponent: React.FC = () => {
+  return (
+    <div>
+      <Gallery />
+    </div>
+  );
+};
+
+export default YourFirstComponent;
