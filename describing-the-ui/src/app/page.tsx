9f9d25c9be2f347a710c5be50f7bcfccdb091a10
mami.teshima <85916526+mami-tus@users.noreply.github.com>
2024-07-11 09:59:44 +0900

    JSX でマークアップを記述する

--- describing-the-ui/src/app/page.tsx
+++ describing-the-ui/src/app/page.tsx
@@ -3,7 +3,16 @@ import React from 'react';
 const Home: React.FC = () => {
   return (
     <div>
-      <h1>Welcome to My Next.js App</h1>
+      <h1>UI の記述</h1>
+      {/* page 作成したらにURLを追加していく */}
+      <ul>
+        <li>
+          <a href='/your-first-component'>初めてのコンポーネント</a>
+        </li>
+        <li>
+          <a href='/writing-markup-with-jsx'>JSX でマークアップを記述する</a>
+        </li>
+      </ul>
     </div>
   );
 };
---
+++ describing-the-ui/src/app/writing-markup-with-jsx/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TodoList from '../components/TodoList';
+
+const YourFirstComponent: React.FC = () => {
+  return (
+    <div>
+      <TodoList />
+    </div>
+  );
+};
+
+export default YourFirstComponent;
