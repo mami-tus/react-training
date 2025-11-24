0406a307edf3931d0e3060b400bf3b8188832ec4
mamitus <85916526+mami-tus@users.noreply.github.com>
2025-11-24 16:18:02 +0900

    Reactドキュメント Quick Start 用ルート作成

---
+++ src/routes/quick-start.tsx
@@ -0,0 +1,14 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/quick-start")({
+	component: () => (
+		<div>
+			<h2>Quick Start</h2>
+			<p>React ドキュメントの Quick Start を写経していく。</p>
+		</div>
+	),
+});
+
+function RouteComponent() {
+	return <div>Hello "/quick-start"!</div>;
+}
