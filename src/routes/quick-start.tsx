0da8ac4942dba3fd6c44f95d541a6d82078eae43
mamitus <85916526+mami-tus@users.noreply.github.com>
2025-11-28 08:30:27 +0900

    feat(router): add /quick-start route

--- src/routes/quick-start.tsx
+++ src/routes/quick-start.tsx
@@ -1,10 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { QuickStartPage } from "../learn/01-quick-start/QuickStartPage";
 
 export const Route = createFileRoute("/quick-start")({
-	component: () => (
-		<div>
-			<h2>Quick Start</h2>
-			<p>React ドキュメントの Quick Start を写経していく。</p>
-		</div>
-	),
+	component: QuickStartPage,
 });
