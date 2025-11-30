e131b2b22750dde6d7bfe994add9c07e7e10733e
mamitus <85916526+mami-tus@users.noreply.github.com>
2025-11-30 09:19:48 +0900

    writing-markup-with-jsx

--- src/learn/01-quick-start/QuickStartPage.tsx
+++ src/learn/01-quick-start/QuickStartPage.tsx
@@ -1,12 +1,15 @@
-import { CreatingAndNestingComponents } from "./CreatingAndNestingComponents";
+import { CreatingAndNestingComponents } from "./components/CreatingAndNestingComponents";
+import { WritingMarkupWithJsx } from "./components/WritingMarkupWithJsx";
 
 export function QuickStartPage() {
 	return (
 		<div>
 			<h1>Quick Start</h1>
+			<br />
 			{/* コンポーネントの作成とネスト */}
 			<CreatingAndNestingComponents />
 			<hr />
+			<WritingMarkupWithJsx />
 			{/* 他のセクションを追加していく */}
 		</div>
 	);
---
+++ src/learn/01-quick-start/components/WritingMarkupWithJsx.tsx
@@ -0,0 +1,21 @@
+function AboutPage() {
+	return (
+		<>
+			<h1>About</h1>
+			<p>
+				Hello there.
+				<br />
+				How do you do?
+			</p>
+		</>
+	);
+}
+
+export function WritingMarkupWithJsx() {
+	return (
+		<>
+			<h2>JSX でマークアップを書く</h2>
+			<AboutPage />
+		</>
+	);
+}
