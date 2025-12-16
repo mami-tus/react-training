a47b35e836a6791b0c4ef8c67f97af1bcf104ae1
mamitus <85916526+mami-tus@users.noreply.github.com>
2025-12-16 22:31:51 +0900

    勝者の宣言

--- src/learn/02-tutorial-tic-tac-toe/components/Board.tsx
+++ src/learn/02-tutorial-tic-tac-toe/components/Board.tsx
@@ -21,7 +21,7 @@ export function Board() {
 	);
 
 	function handleClick(i: number) {
-		if (squares[i]) {
+		if (squares[i] || calculateWinner(squares)) {
 			return;
 		}
 		const nextSquares = squares.slice();
@@ -34,8 +34,15 @@ export function Board() {
 		setXIsNext(!xIsNext);
 	}
 
+	const winner = calculateWinner(squares);
+	const status =
+		winner !== null
+			? `Winner: ${winner}`
+			: `Next player: ${xIsNext ? "X" : "O"}`;
+
 	return (
 		<>
+			<div className="status">{status}</div>
 			<div className="board-row">
 				<Square value={squares[0]} onSquareClick={() => handleClick(0)} />
 				<Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -53,4 +60,24 @@ export function Board() {
 			</div>
 		</>
 	);
+}
+
+function calculateWinner(squares: (string | null)[]) {
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+	for (let i = 0; i < lines.length; i++) {
+		const [a, b, c] = lines[i];
+		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+			return squares[a];
+		}
+	}
+	return null;
 }
